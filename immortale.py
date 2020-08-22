261ef99433f6e0d578b90acbaeaab9e9eb31b3fe
borzo <borzoneg@gmail.com>
2020-08-22 23:56:26 +0200

    Update

--- immortale.py
+++ immortale.py
@@ -8,20 +8,27 @@ angles=[90, 90, 90]
 for i in range(3):
 	kit.servo[i].angle = angles[i]
 
+def moveServo(servo, currentAngle, targetAngle):
+	while(currentAngle != targetAngle):
+		currentAngle += 5
+		servo.angle(currentAngle)
+		time.wait(0.5)
 
-def straight():
-	kit.servo[1].angle = 70
-	kit.servo[0].angle = 120
-	kit.servo[2].angle = 130
-	kit.servo[2].angle = 90
-	kit.servo[1].angle = 90
-	kit.servo[0].angle = 90
 
+def straight(leg):
+	moveServo(kit.servo[leg+1], 90, 70)
+	moveServo(kit.servo[leg+0], 90, 120)
+	moveServo(kit.servo[leg+2], 90, 130)
+	moveServo(kit.servo[leg+2], 70, 90)
+	moveServo(kit.servo[leg+1], 120, 90)
+	moveServo(kit.servo[leg+0], 130, 90)
+
+kit.servo[i].set_pulse_width_range(1000)
 
 while True:
 	comand = input()
 	if(comand=='l'):
-		straight()
+		straight(0)
 	if(comand=='q'):
 		angles[0] += 5
 		kit.servo[0].angle = angles[0]
